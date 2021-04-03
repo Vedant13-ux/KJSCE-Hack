49a2636b155aa715fad3a0463a71220166d49448
CodeWizard99 <gohilumang99@gmail.com>
2021-04-03 23:22:34 +0530

    footer

--- frontend/src/components/Landing.js
+++ frontend/src/components/Landing.js
@@ -3,7 +3,7 @@ import Nav from '../containers/global/Nav';
 import About from '../containers/global/About';
 import QnA from './QnA';
 import '../index2.css';
-import BlogContent from './BlogContent';
+import Footer from '../containers/global/Footer';
 
 
 function Landing(){
@@ -12,7 +12,7 @@ function Landing(){
             <Nav />
             <About />
             <QnA />
-            
+            <Footer />
         </div>
     )
 }
