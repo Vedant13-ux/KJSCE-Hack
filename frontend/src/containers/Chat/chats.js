ac2ee3231d5f5fc3b4341eb916800b7c876aaec3
RedHeadphone <khilawala92@gmail.com>
2021-04-04 01:40:29 +0530

    done

--- frontend/src/containers/Chat/chats.js
+++ frontend/src/containers/Chat/chats.js
@@ -1,30 +1,42 @@
-import React from 'react'
-import Navbar from '../global/Nav'
-import {apiCall} from '../../services/api'
+import React from "react";
+import Navbar from "../global/Nav";
+import { apiCall } from "../../services/api";
+import {Link} from 'react-router-dom'
 
 export default class ChatApp extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            appointments:[]
-        };
-    }
-    componentDidMount(){
-        apiCall('get','appointment/getall/'+this.props.uid).then(v=>{
-            console.log(v)
-            this.setState({appointments:v})
-        })
-    }
-    render() {
-        return (
-            <div id="chat">
-                <Navbar></Navbar>
-                <div className="app">
-                    {this.state.map((e,i)=>{
-                        return <div></div>
-                    })}
+  constructor(props) {
+    super(props);
+    this.state = {
+      appointments: [],
+    };
+  }
+  componentDidMount() {
+    apiCall("get", "appointment/getall/" + this.props.uid).then((v) => {
+      console.log(v);
+      this.setState({ appointments: v });
+    });
+  }
+  render() {
+    return (
+      <div id="chat">
+        <Navbar></Navbar>
+        <div className="app" id="experience">
+          {this.state.appointments.map((e, i) => {
+            return (
+              <Link to={"/chat/"+e._id}  style={{ textDecoration: 'none' }}>
+                <div className="experience-ele project-ele">
+                  <h4 style={{margin:"0px",padding:"0px"}}>
+                    {"Appointment at "+e.time}
+                  </h4>
+                  <p style={{margin:"0px",padding:"0px"}}>
+                    {new Date(e.date).toDateString()}
+                  </p>
                 </div>
-            </div>
-        );
-    }
+              </Link>
+            );
+          })}
+        </div>
+      </div>
+    );
+  }
 }
