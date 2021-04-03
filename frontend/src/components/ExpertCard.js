a7ae3e8b09654320c9b596285cfcf46aa7a63a07
CodeWizard99 <gohilumang99@gmail.com>
2021-04-04 02:15:12 +0530

    Course Card Size fixed

--- frontend/src/components/ExpertCard.js
+++ frontend/src/components/ExpertCard.js
@@ -10,11 +10,12 @@ import CardActions from '@material-ui/core/CardActions';
 import Avatar from '@material-ui/core/Avatar';
 import IconButton from '@material-ui/core/IconButton';
 import Typography from '@material-ui/core/Typography';
-import { red } from '@material-ui/core/colors';
+import { lightGreen, red } from '@material-ui/core/colors';
 import FavoriteIcon from '@material-ui/icons/Favorite';
 import ShareIcon from '@material-ui/icons/Share';
 import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 import MoreVertIcon from '@material-ui/icons/MoreVert';
+import { light } from '@material-ui/core/styles/createPalette';
 
 const useStyles = makeStyles((theme) => ({
   root: {
