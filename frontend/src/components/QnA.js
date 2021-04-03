271c605254590ba7e6388c3ebe04610e6c073c4e
CodeWizard99 <gohilumang99@gmail.com>
2021-04-03 20:00:56 +0530

    Listener issue fixed and Courses added

---
+++ frontend/src/components/QnA.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import SimpleAccordion from './SimpleAccordian';
+import '../index2.css';
+
+function QnA() {
+    return (
+        <div className="qna">
+            <h1 className="qna_title">Frequently Asked Questions:-  </h1>
+            <SimpleAccordion />
+        </div>
+    )
+}
+
+export default QnA
