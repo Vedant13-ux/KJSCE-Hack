42e1ae46468561e5ce296354beb8a842592f4975
Rajas-B <bondalerajas@gmail.com>
2021-04-03 23:20:44 +0530

    Refined courses

--- frontend/src/components/Course.js
+++ frontend/src/components/Course.js
@@ -12,19 +12,29 @@ function Course() {
             "description" : "Different tips & tricks to manage your stress in an efficient manner."
         },
         {
-            "title":"Reduce Stress",
-            "path" : "https://youtu.be/YhpU8VME8Gw",
-            "description" : "Different tips & tricks to manage your stress in an efficient manner."
+            "title":"Bullying in Schools: Classroom Lesson",
+            "path" : "https://www.youtube.com/watch?v=2meoVOc-RxU",
+            "description" : "A lesson for students, teachers, parents, administrators and more to get a basic understanding on what exactly is bullying in schools."
         },
         {
-            "title":"Reduce Stress",
-            "path" : "https://youtu.be/YhpU8VME8Gw",
-            "description" : "Different tips & tricks to manage your stress in an efficient manner."
+            "title":"Jordan Peterson - How to Deal With Bullies And Confronting Your Fears",
+            "path" : "https://www.youtube.com/watch?v=yZ8VXb-95vg",
+            "description" : "In this lecture, Dr. Jordan Peterson talks about how to stop bullying before it escalates full scale. "
         },
         {
-            "title":"Reduce Stress",
-            "path" : "https://youtu.be/YhpU8VME8Gw",
-            "description" : "Different tips & tricks to manage your stress in an efficient manner."
+            "title":"How I survived workplace bullying",
+            "path" : "https://www.youtube.com/watch?v=YmRKlZEXVQM",
+            "description" : "What happens when you’re on the bullying end or an organization meant to protect people?  Who do you go to for help? "
+        },
+        {
+            "title":"Jordan Peterson : How he treats ANXIETY",
+            "path" : "https://www.youtube.com/watch?v=ijdr4eJcdHQ",
+            "description" : "Listen to the world famous psychologist and his methods to treat anxiety"
+        },
+        {
+            "title":"Ways to stop bullying",
+            "path" : "https://www.youtube.com/watch?v=ynTuA_tlZDE",
+            "description" : "Are you being bullied?"
         }
     ]
     return (
