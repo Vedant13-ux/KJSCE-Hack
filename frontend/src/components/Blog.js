9137f0ccbec70a9034dab1fd3448f99f032dddef
Vedant13-ux <56965523+Vedant13-ux@users.noreply.github.com>
2021-04-04 10:05:43 +0530

    d

--- frontend/src/components/Blog.js
+++ frontend/src/components/Blog.js
@@ -4,33 +4,49 @@ import '../index2.css';
 import Nav from '../containers/global/Nav';
 import Footer from '../containers/global/Footer'
 import FloatingButtons from './FloatingButton';
+import { apiCall } from '../services/api'
 
 
-function Blog(props) {
-    return (
-        <div className="course">
-            <Nav user={props.user} logout={props.logout} />
-            <div className="course_wrap">
-                <div className="course_title">
-                    <h1>----- Blog -----</h1>
+class Blog extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            blogs: []
+        }
+    }
+    componentDidMount() {
+        apiCall('get', '/blog/allBlogs', '')
+            .then((blogs) => {
+                this.setState({ blogs })
+            }).catch((err) => {
+                console.log(err);
+            });
+    }
+    render() {
+        return (
+            <div className="course" >
+                <Nav user={this.props.user} logout={this.props.logout} />
+                <div className="course_wrap">
+                    <div className="course_title">
+                        <h1>----- Blog -----</h1>
+                    </div>
+
+                    <div className="course_card">
+                        {this.state.blogs.map(blog => {
+                            return <div className="blogCard"><BlogCard key={blog._id} blog={blog} /></div>
+                        })}
+                        <div className="add_icon"><FloatingButtons /></div>
+
+
+                    </div>
+
                 </div>
 
-                <div className="course_card">
-                    <div className="blogCard"><BlogCard /></div>
-                    <div className="blogCard"><BlogCard /></div>
-                    <div className="blogCard"><BlogCard /></div>
-                    <div className="blogCard"><BlogCard /></div>
-                    <div className="blogCard"><BlogCard /></div>
-                    <div className="blogCard"><BlogCard /></div>
-                    <div className="add_icon"><FloatingButtons /></div>
-                </div>
 
+                <Footer />
             </div>
-
-
-            <Footer />
-        </div>
-    )
+        )
+    }
 }
 
 export default Blog
