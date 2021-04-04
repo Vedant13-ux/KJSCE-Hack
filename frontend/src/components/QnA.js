24404b55c7d298e6a1e59af46c023eb9be0c3350
CodeWizard99 <gohilumang99@gmail.com>
2021-04-04 10:24:53 +0530

    FAQ fixed

--- frontend/src/components/QnA.js
+++ frontend/src/components/QnA.js
@@ -1,12 +1,23 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import SimpleAccordion from './SimpleAccordian';
 import '../index2.css';
+import Nav from '../containers/global/Nav';
+import Footer from '../containers/global/Footer';
 
-function QnA() {
+function QnA(props) {
+
+    useEffect(() => {
+        window.scrollTo(0, 0);
+    });
+
     return (
-        <div id="faq" className="qna">
-            <h1 className="qna_title">Frequently Asked Questions:-  </h1>
-            <SimpleAccordion />
+        <div className="faq_container">
+            <Nav user={props.user} logout={props.logout} />
+            <div id="faq" className="qna">
+                <h1 className="qna_title">Frequently Asked Questions:-  </h1>
+                <SimpleAccordion />
+            </div>
+            <Footer />
         </div>
     )
 }
